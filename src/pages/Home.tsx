a127c65417f242cdd4894dfe811b071316f764ad
elliesbang <shandbr1996@gmail.com>
2025-10-23 19:10:08 +0900

    feat: add subscription plans modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import EditorSection from '../components/EditorSection'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import ModalLogin from '../components/ModalLogin'
+import ModalSubscriptionPlans from '../components/ModalSubscriptionPlans'
 import ResultSection from '../components/ResultSection'
 import UploadSection from '../components/UploadSection'
 import UtilityPanel from '../components/UtilityPanel'
@@ -10,12 +11,13 @@ import Toast from '../components/Toast'
 
 function Home() {
   const [isLoginOpen, setIsLoginOpen] = useState(false)
+  const [isUpgradeOpen, setIsUpgradeOpen] = useState(false)
 
   return (
     <div className="min-h-screen bg-ellie-ivory text-ellie-text">
       <Header
         onLoginClick={() => setIsLoginOpen(true)}
-        onUpgradeClick={() => setIsLoginOpen(true)}
+        onUpgradeClick={() => setIsUpgradeOpen(true)}
       />
       <main className="mx-auto flex w-full max-w-5xl flex-col gap-10 px-4 pb-36 pt-24 sm:px-6 lg:px-8">
         <UploadSection />
@@ -26,6 +28,11 @@ function Home() {
       <Footer />
       <Toast />
       <ModalLogin open={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
+      <ModalSubscriptionPlans
+        open={isUpgradeOpen}
+        onClose={() => setIsUpgradeOpen(false)}
+        currentPlanId="free"
+      />
     </div>
   )
 }
