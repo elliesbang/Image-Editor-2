d1da25c6b2367e37a48747bded002401203afaaf
elliesbang <shandbr1996@gmail.com>
2025-10-23 15:52:19 +0900

    Configure Amplify build to use npm install

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    emptyOutDir: true
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 })
